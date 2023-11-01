eae4bbb8686ba47c7c8994f8bbd521dd3b639483
melissa-24 <mlongenberger@codingdojo.com>
2023-11-01 18:05:51 -0400

    saving.

--- lectures/week06/day1/server.py
+++ lectures/week06/day1/server.py
@@ -22,16 +22,5 @@ def room2(name):
 
 
 
-
-
-
-
-
-
-
-
-
-
-
 if __name__ == "__main__":  # Ensure this file is being run directly and not from a different module  
     app.run(debug=True)   # Run the app in debug mode.
